40ad4f70af71ba936b41223f23549aad48983e3b
bxsmtj <smtjong@bxtheory.com>
2022-09-21 17:01:23 -0400

    cases - update dropdown for wide class

--- dist/cases/script.js
+++ dist/cases/script.js
@@ -3,16 +3,16 @@ gsap.registerPlugin(ScrollTrigger);
 $(document).ready(function () {
 	let elWrapper = document.getElementById('wrapper__select--cases');
 	let elSelect = document.createElement('select');
-	// elSelect.classList.add('wide');
+	elSelect.classList.add('wide');
 	const dataOptions = [
-		'Manufacturing Company with $131M Debt, bankrupt in 2022',
-		'Retail Company with $256M Debt, bankrupt in 2020',
-		'Oil & Gas Company with $532M Debt, bankrupt in 2020',
-		'Transportation Company with $381M Debt, bankrupt in 2019',
-		'Retail Company with $1,357M Debt, bankrupt in 2018',
-		'Retail Company with $841.5M Debt, bankrupt in 2015',
-		'Electronics Company with $91M Debt, bankrupt in 2008',
-		'Textiles Company with $33M Debt, bankrupt in 2007',
+		'1: Manufacturing Company with $131M Debt, bankrupt in 2022',
+		'2: Retail Company with $256M Debt, bankrupt in 2020',
+		'3: Oil & Gas Company with $532M Debt, bankrupt in 2020',
+		'4: Transportation Company with $381M Debt, bankrupt in 2019',
+		'5: Retail Company with $1,357M Debt, bankrupt in 2018',
+		'6: Retail Company with $841.5M Debt, bankrupt in 2015',
+		'7: Electronics Company with $91M Debt, bankrupt in 2008',
+		'8: Textiles Company with $33M Debt, bankrupt in 2007',
 	];
 	dataOptions.forEach((d, i) => {
 		let elOption = document.createElement('option');
