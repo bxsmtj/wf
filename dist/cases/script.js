c2127dc80f0f1d75d899a7a089432c0755c0d8ad
bxsmtj <smtjong@bxtheory.com>
2022-09-21 16:57:32 -0400

    cases - update dropdown

--- dist/cases/script.js
+++ dist/cases/script.js
@@ -3,15 +3,19 @@ gsap.registerPlugin(ScrollTrigger);
 $(document).ready(function () {
 	let elWrapper = document.getElementById('wrapper__select--cases');
 	let elSelect = document.createElement('select');
-	elSelect.classList.add('wide');
+	// elSelect.classList.add('wide');
 	const dataOptions = [
-		'Manufacturing Company with $131M Debt, bankrupt in May 2022',
-		'Retail Company with $256M Debt, bankrupt in December 2020',
-		'Oil & Gas Company with $532M Debt, bankrupt in July 2020',
+		'Manufacturing Company with $131M Debt, bankrupt in 2022',
+		'Retail Company with $256M Debt, bankrupt in 2020',
+		'Oil & Gas Company with $532M Debt, bankrupt in 2020',
+		'Transportation Company with $381M Debt, bankrupt in 2019',
+		'Retail Company with $1,357M Debt, bankrupt in 2018',
+		'Retail Company with $841.5M Debt, bankrupt in 2015',
+		'Electronics Company with $91M Debt, bankrupt in 2008',
+		'Textiles Company with $33M Debt, bankrupt in 2007',
 	];
 	dataOptions.forEach((d, i) => {
 		let elOption = document.createElement('option');
-
 		elOption.value = i;
 		elOption.innerText = d;
 		elSelect.appendChild(elOption);
