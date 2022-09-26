f39fe3be708b0eec0a037a3d46f9b9b3d48136f2
bxsmtj <smtjong@bxtheory.com>
2022-09-25 21:50:21 -0400

    tooltips - set up

---
+++ dist/tooltips/home/script.js
@@ -0,0 +1,9 @@
+tippy('.tippy', {          // Add the class tippy to your element
+	theme: 'light',          // Dark or Light
+	animation: 'scale',      // Options, shift-away, shift-toward, scale, persepctive
+	duration: 250,           // Duration of the Animation
+	arrow: true,             // Add arrow to the tooltip
+	arrowType: 'round',      // Sharp, round or empty for none
+	delay: [0, 50],          // Trigger delay in & out
+	maxWidth: 240,           // Optional, max width settings
+})
