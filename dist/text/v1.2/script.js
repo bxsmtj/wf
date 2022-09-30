18da0afe3cdbe219a84c35808e29b6795ad35a24
bxsmtj <smtjong@bxtheory.com>
2022-09-30 10:35:16 -0400

    text - comment out blinking underscore to try fixing glitchy

--- dist/text/v1.2/script.js
+++ dist/text/v1.2/script.js
@@ -2,86 +2,90 @@ let windowWidth = window.outerWidth;
 gsap.registerPlugin(SplitText);
 gsap.registerPlugin(ScrollTrigger);
 
-$(document).ready(function () {
-	$('.split').each(function () {
-		let myText = $(this);
-		let mySplitText;
-		function createSplits() {
-			mySplitText = new SplitText(myText, {
-				type: 'chars,words,lines',
-				charsClass: 'split-chars',
-				wordsClass: 'split-words',
-				linesClass: 'split-lines',
-			});
+// setTimeout(function () {
+$('.split').each(function () {
+	let myText = $(this);
+	let mySplitText;
+	function createSplits() {
+		mySplitText = new SplitText(myText, {
+			type: 'chars,words,lines',
+			charsClass: 'split-chars',
+			wordsClass: 'split-words',
+			linesClass: 'split-lines',
+		});
+	}
+	createSplits();
+	$(window).resize(function () {
+		if (window.outerWidth !== windowWidth) {
+			mySplitText.revert();
+			location.reload();
 		}
-		createSplits();
-		$(window).resize(function () {
-			if (window.outerWidth !== windowWidth) {
-				mySplitText.revert();
-				location.reload();
-			}
-			windowWidth = window.outerWidth;
+		windowWidth = window.outerWidth;
+	});
+});
+// }, 200);
+
+function initializeTypedAnimationsH() {
+	$('.typed--h').each(function () {
+		let triggerElement = $(this);
+		let targetElement = $(this).find('.split-chars');
+
+		let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: triggerElement,
+			},
+		});
+		tl.from(targetElement, {
+			duration: 0.02,
+			y: '0%',
+			opacity: 0.01,
+			ease: 'power1.none',
+			stagger: {
+				each: 0.05,
+				from: '0',
+				// onStart: function () {
+				// 	$(this.targets()[0]).addClass('cursor__static');
+				// },
+				// onComplete: function () {
+				// 	$(this.targets()[0]).removeClass('cursor__static');
+				// },
+			},
+			// onComplete: function () {
+			// 	$(this.targets()[this.targets().length - 1]).addClass('cursor__blink');
+			// },
 		});
 	});
+}
 
-	function initializeTypedAnimationsH() {
-		$('.typed--h').each(function () {
-			let triggerElement = $(this);
-			let targetElement = $(this).find('.split-chars');
+function initializeTypedAnimationsP() {
+	$('.typed--p').each(function () {
+		let triggerElement = $(this);
+		let targetElement = $(this).find('.split-chars');
 
-			let tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: triggerElement,
-				},
-			});
-			tl.from(targetElement, {
-				duration: 0.02,
-				opacity: 0,
-				ease: 'power1.none',
-				stagger: {
-					each: 0.05,
-					from: '0',
-					onStart: function () {
-						$(this.targets()[0]).addClass('cursor__static');
-					},
-					onComplete: function () {
-						$(this.targets()[0]).removeClass('cursor__static');
-					},
-				},
-				onComplete: function () {
-					$(this.targets()[this.targets().length - 1]).addClass(
-						'cursor__blink'
-					);
-				},
-			});
+		let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: triggerElement,
+			},
 		});
-	}
-
-	function initializeTypedAnimationsP() {
-		$('.typed--p').each(function () {
-			let triggerElement = $(this);
-			let targetElement = $(this).find('.split-chars');
-
-			let tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: triggerElement,
-				},
-			});
-			tl.from(targetElement, {
-				delay: 0.5,
-				duration: 0.02,
-				opacity: 0.1,
-				ease: 'power1.none',
-				stagger: {
-					each: 0.05,
-					from: '0',
-				},
-			});
+		tl.from(targetElement, {
+			delay: 0.5,
+			y: '0%',
+			duration: 0.02,
+			opacity: 0.1,
+			ease: 'power1.none',
+			stagger: {
+				each: 0.05,
+				from: '0',
+			},
 		});
-	}
+	});
+}
 
+$(document).ready(function () {
+	// setTimeout(function () {
 	initializeTypedAnimationsH();
 	initializeTypedAnimationsP();
+	// }, 250);
 
 	const resizeElements = document.querySelectorAll('.section');
 	const resizeObserver = new ResizeObserver((entries) => {
