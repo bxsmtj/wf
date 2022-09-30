82ced2e4080ad1f3e64cbf3002b05f59aa9e1584
bxsmtj <smtjong@bxtheory.com>
2022-09-30 16:23:53 -0400

    galton - initial version of matterjs w/ bounds

---
+++ dist/galton/script.js
@@ -0,0 +1,156 @@
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+window.addEventListener('resize', function () {
+	width = window.innerWidth;
+	height = window.innerHeight;
+});
+
+function percentX(percent) {
+	return Math.round((percent / 100) * windowWidth);
+}
+function percentY(percent) {
+	return Math.round((percent / 100) * windowHeight);
+}
+
+var Example = Example || {};
+
+Example.galton = function () {
+	var Engine = Matter.Engine,
+		Render = Matter.Render,
+		Runner = Matter.Runner,
+		World = Matter.World,
+		Bodies = Matter.Bodies;
+	Bounds = Matter.Bounds;
+
+	// create engine
+	var engine = Engine.create({
+			enableSleeping: true,
+		}),
+		world = engine.world;
+
+	// create renderer
+	var render = Render.create({
+		// element: document.body,
+		element: document.getElementById('wrapper__galton-1'),
+		engine: engine,
+		options: {
+			width: 520,
+			height: 347,
+			// width: percentX(100),
+			// height: (percentX(100) / 3) * 2,
+			wireframes: false,
+			background: '#181B18',
+			hasBounds: true,
+		},
+	});
+
+	Render.run(render);
+
+	// create runner
+	var runner = Runner.create();
+	Runner.run(runner, engine);
+
+	const size = 4;
+
+	// add bodies
+	let total = 1400;
+	setInterval(() => {
+		if (total-- > 0) {
+			const circle = Bodies.rectangle(
+				250 + (-0.5 + Math.random()),
+				-20,
+				size,
+				size,
+				{
+					friction: 0.00001,
+					restitution: 0.5,
+					density: 0.001,
+					frictionAir: 0.042,
+					sleepThreshold: 25,
+					render: {
+						fillStyle: '#ffffff',
+						visible: true,
+					},
+				}
+			);
+
+			Matter.Events.on(circle, 'sleepStart', () => {
+				Matter.Body.setStatic(circle, true);
+			});
+			World.add(world, circle);
+		}
+	}, 10);
+
+	const pegs = [];
+	const spacingY = 35;
+	const spacingX = 40;
+	var i, j, lastI;
+	for (i = 0; i < 13; i++) {
+		for (j = 1; j < i; j++) {
+			pegs.push(
+				Bodies.circle(
+					250 + (j * spacingX - i * (spacingX / 2)),
+					i * spacingY,
+					size,
+					{
+						isStatic: true,
+						render: {
+							fillStyle: '#ffffff',
+							visible: false,
+						},
+					}
+				)
+			);
+		}
+		lastI = i;
+	}
+	for (i = 0; i < 15; i++) {
+		World.add(
+			world,
+			Bodies.rectangle(
+				110 - spacingX + (j * spacingX - i * spacingX),
+				lastI * spacingY + 215,
+				size / 2,
+				lastI + 300,
+				{
+					isStatic: true,
+					render: {
+						fillStyle: '#ffffff',
+						visible: false,
+					},
+					chamfer: {
+						radius: [size * 0.4, size * 0.4, 0, 0],
+					},
+				}
+			)
+		);
+	}
+	World.add(
+		world,
+		Bodies.rectangle(250, lastI * 1.33 * spacingY + 257, 1000, 50, {
+			isStatic: true,
+			render: {
+				fillStyle: '#ffffff',
+				visible: false,
+			},
+		})
+	);
+
+	World.add(world, pegs);
+
+	Bounds.translate(render.bounds, { x: 0, y: 800 - 347 });
+
+	return {
+		engine: engine,
+		runner: runner,
+		render: render,
+		canvas: render.canvas,
+		stop: function () {
+			Matter.Render.stop(render);
+			Matter.Runner.stop(runner);
+		},
+	};
+};
+
+Example.galton();
